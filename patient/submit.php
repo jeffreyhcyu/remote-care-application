df144ccc6a3159fcea1f268b8c7408bb97e86127
henryfletch <henry@villocq.com>
2014-01-23 11:43:05 +0000

    Major changes: Recoded for MySQLi

--- patient/submit.php
+++ patient/submit.php
@@ -1,14 +1,17 @@
 <?php
-session_start();
+
 // Php code for handling the BP submission form
 
+session_start();
+
 // Configure the MySQL connection
+$server='remote.villocq.com';
 $username="3yp";
 $DBpassword="project";
 $database="tallis";
 
-mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
-@mysql_select_db($database) or die("Error! Something bad happened!");
+// New MySQLi Instance
+$db = new mysqli($server,$username,$DBpassword,$database);
 
 //Assign the form POST output to PHP variables
 $id=$_SESSION['userID'];
@@ -16,27 +19,55 @@ $input_password=$_SESSION['userPassword'];
 $sysBP=$_POST['sysBP'];
 $diaBP=$_POST['diaBP'];
 
-//Insert the form output into a new row of the patientCurrentBP table
-$enterBPquery="INSERT INTO patientCurrentBP VALUES ('',now(),AES_ENCRYPT('$id','$input_password'),AES_ENCRYPT('$sysBP','$input_password'),AES_ENCRYPT('$diaBP','$input_password'))";
-mysql_query($enterBPquery);
+//Prepared statement
+$submit = $db->prepare("INSERT INTO patientCurrentBP VALUES ('',now(),AES_ENCRYPT(?,?),AES_ENCRYPT(?,?),AES_ENCRYPT(?,?))");
+
+//Bind & Execute to submit to database
+$submit->bind_param('ssssss',$id,$input_password,$sysBP,$input_password,$diaBP,$input_password);
+$submit->execute();
+// Data has now been submitted!
 
 
-//Below code handles the BP target checking
-//
-//Check the patient's target Systolic BP
-$getSystolic="SELECT AES_DECRYPT(patientTargetBPSystolic,'$input_password') FROM patientTargetBP WHERE AES_DECRYPT(patientID,'$input_password')='$id'";
-$targetSystolic=mysql_result(mysql_query($getSystolic),0);
+//// Below code handles the BP target checking ////
 
-//Check the patient's target Diastolic BP
-$getDiastolic="SELECT AES_DECRYPT(patientTargetBPDiastolic,'$input_password') FROM patientTargetBP WHERE AES_DECRYPT(patientID,'$input_password')='$id'";
-$targetDiastolic=mysql_result(mysql_query($getDiastolic),0);
+//Get the patient's target Systolic BP:
+//Prepared statement
+$getSystolic = $db->prepare("SELECT AES_DECRYPT(patientTargetBPSystolic,?) FROM patientTargetBP WHERE AES_DECRYPT(patientID,?)=?");
+//Execute and get result
+$getSystolic->bind_param('sss',$input_password,$input_password,$id);
+$getSystolic->execute();
+$getSystolic->bind_result($targetSystolic);
+$getSystolic->fetch();
 
-//Date checking. $DaysElapsed is days betwwen first BP reading in the database and today
+//Get the patient's target Diastolic BP:
+//Prepared statement
+$getDiastolic = $db->prepare("SELECT AES_DECRYPT(patientTargetBPDiastolic,?) FROM patientTargetBP WHERE AES_DECRYPT(patientID,?)=?");
+//Execute and get result
+$getDiastolic->bind_param('sss',$input_password,$input_password,$id);
+$getDiastolic->execute();
+$getDiastolic->bind_result($targetDiastolic);
+$getDiastolic->fetch();
+
+
+//Date checking. $DaysElapsed is days betwwen first BP reading in the database and today.
 // ==>> NB! Need to think of a way to reset the count when secondary treatment is enacted!
-$getDate="SELECT min(date) FROM patientCurrentBP WHERE patientID=AES_ENCRYPT('$id','$input_password')";
-$earliestDate=mysql_result(mysql_query($getDate),0);
-$dateDifference="SELECT DATEDIFF(now(),'$earliestDate') AS date_difference";
-$daysElapsed=mysql_result(mysql_query($dateDifference),0);
+//Get earliest date that data was submitted:
+//Prepared statement
+$getDate = $db->prepare("SELECT min(date) FROM patientCurrentBP WHERE patientID=AES_ENCRYPT(?,?)");
+//Execute & get
+$getDate->bind_param('ss',$id,$input_password);
+$getDate->execute();
+$getDate->bind_result($earliestDate);
+$getDate->fetch();
+
+//Now get a difference in days between earliestDate and today:
+//Prepared statement
+$dateDiff = $db->prepare("SELECT DATEDIFF(now(),?) AS date_difference");
+//Execute & get
+$dateDiff->bind_param('s',$earliestDate);
+$dateDiff->execute();
+$dateDiff->bind_result($daysElapsed);
+$dateDiff->fetch();
 
 
 //Decision Logic for the target BP checking
@@ -56,7 +87,6 @@ else
 	{  
 	echo "Your Systolic BP is too high.";
 	echo "<br>";
-	mysql_query("UPDATE patientTargetBP SET flag='1' WHERE AES_DECRYPT(patientID,'$input_password')='$id'");
         $flag = 1;
 	}
     
@@ -65,25 +95,33 @@ else
 	{  
 	echo "Your Diastolic BP is too high.";
 	echo "<br>";
-	mysql_query("UPDATE patientTargetBP SET flag='1' WHERE AES_DECRYPT(patientID,'$input_password')='$id'");
-        $flag=1;
+        $flag = 1;
 	}
 
     //If BP is lower than target on both:
     if ($targetSystolic >= $sysBP AND $targetDiastolic >= $diaBP)
 	{
-	mysql_query("UPDATE patientTargetBP SET flag='0' WHERE AES_DECRYPT(patientID,'$input_password')='$id'");
-        $flag=0;
-	header("Location: https://3yp.villocq.com/patient/continue_Cardiac_track2.html"); 
-    }
-
-    //If a flag is set for high BP, refer to doctor
-    elseif ($flag=1)
+        $flag = 0;
+        }
+    
+    //Now submit the flag to the database:
+    //Prepared statement
+    $setFlag = $db->prepare("UPDATE patientTargetBP SET flag=? WHERE AES_DECRYPT(patientID,?)=?");
+    //Execute
+    $setFlag->bind_param('iss',$flag,$input_password,$id);
+    $setFlag->execute();
+    
+    //If a flag is set for high BP, refer to doctor. Otherwise send to 'success' page:
+    if ($flag=1)
+        { 
+        header("Location: https://3yp.villocq.com/patient/alert.php"); //Send to alert page
+        }
+    else
         {
-        header("Location: https://3yp.villocq.com/patient/alert.php"); 
+        header("Location: https://3yp.villocq.com/patient/continue_Cardiac_track2.html"); //Send to continue page
         }
     }
     
 //Close the DB connection
-mysql_close();
+$db->close();
 ?>
