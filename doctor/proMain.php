80433bc2442dc92d6176008083e9337e669612f3
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-01-30 12:43:33 +0000

    add patient id

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -29,13 +29,13 @@ mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
 @mysql_select_db($database);
 
 
-$result = mysql_query("SELECT patientID, ageGroup FROM patientInfo");
+$result = mysql_query("SELECT patientID, ageGroup, id FROM patientInfo");
 $num = mysql_num_rows($result);
 
 while($row = mysql_fetch_array($result))
   {
   echo '<div class="Apatient">';
-  echo $row['patientID'] . "    age:" . $row['ageGroup'];
+  echo $row['patientID'] . " id:" . $row['id']. " age:" . $row['ageGroup'];
   echo '</div>';
   }
 
