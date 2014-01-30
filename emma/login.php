fe83b17380132519088bd206ddf1937d8e28ea96
esaragossi <emma@octa.co.uk>
2014-01-30 20:28:35 +0000

    Create login.php

---
+++ emma/login.php
@@ -0,0 +1,59 @@
+<?php
+// Login Form
+
+// enable sessions
+session_start();
+
+// Configure the MySQL connection parameters
+$server='remote.villocq.com';
+$username='3yp';
+$DBpassword='project';
+$database='tallis';
+
+// New MySQLi Instance
+$db = new mysqli($server,$username,$DBpassword,$database);
+
+if ($db->connect_error) {
+  trigger_error('Database connection failed: '  . $db->connect_error, E_USER_ERROR); // Error message if fails
+}
+
+//Assign the login form POST output to PHP variables
+$id=$_POST['username'];
+$input_password=$_POST['password'];
+
+// Get and compare the hashed passwords:
+// Prepared statement
+$getHash = $db->prepare("SELECT patientPassword FROM patientTargetBP WHERE patientID=?");
+if($getHash === false) {
+  trigger_error('SQL Statement Error: ' . $db->error, E_USER_ERROR); // Error message if fails
+}
+
+// Bind & Execute
+$getHash->bind_param('s',$id);
+$getHash->execute();
+$getHash->bind_result($actual_password_hash);
+$getHash->fetch();
+
+// Check password matches, using PHPass here
+require("PasswordHash.php");
+$hasher = new PasswordHash(10,false); // 10 is the cost function setting
+$checkPassword = $hasher->CheckPassword($input_password,$actual_password_hash);
+
+// If good password:
+if ($checkPassword)  
+{
+    $_SESSION['userID'] = $id;
+    $_SESSION['loginMessage'] = '';
+    
+header('Location: index.html'); 
+}
+else
+{
+    $_SESSION['loginMessage'] = 'Login Error';
+    header('Location: loginPage.php'); 
+}
+
+$db->close();
+
+?>
+    
