c22fa4f3cf3fa840d0453fc5be6ce07b419f5f69
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-01-30 11:15:32 +0000

    updated mysql

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -19,29 +19,29 @@ Patients
 Alerted
 </div>
 
+
 <?php
 $username="3yp";
 $DBpassword="project";
 $database="tallis";
 
-mysql_connect("remote.villocq.com:3306",$username,$DBpassword);
+mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
 @mysql_select_db($database);
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
-$result = mysqli_query($con,"SELECT patientID, ageGroup FROM patientInfo");
 
-while($row = mysqli_fetch_array($result))
+$result = mysql_query("SELECT patientID, ageGroup FROM patientInfo");
+$num = mysql_num_rows($result);
+
+echo $num;
+
+while($row = mysql_fetch_array($result))
   {
-  //echo '<div class="Apatient"';
+  echo '<div class="Apatient"';
   echo $row['patientID'] . " " . $row['ageGroup'];
-  //echo '</div>';
+  echo '</div>';
   }
 
-mysqli_close($con);
+mysql_close();
 ?>
 
 </div>
