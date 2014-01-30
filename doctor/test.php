0ff3334335d781209ae1eeb60c8b809008f0fbbc
henryfletch <henry@villocq.com>
2014-01-30 11:08:04 +0000

    Removed mysqli
    cannot mix mysql with mysqli

--- doctor/test.php
+++ doctor/test.php
@@ -5,23 +5,19 @@ $database="tallis";
 
 mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
 @mysql_select_db($database);
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
-$result = mysqli_query("SELECT patientID, ageGroup FROM patientInfo");
+
+$result = mysql_query("SELECT patientID, ageGroup FROM patientInfo");
 $num = mysql_num_rows($result);
 
 echo $num;
 
-while($row = mysqli_fetch_array($result))
+while($row = mysql_fetch_array($result))
   {
   //echo '<div class="Apatient"';
   echo $row['patientID'] . " " . $row['ageGroup'];
   //echo '</div>';
   }
 
-mysqli_close();
+mysql_close();
 ?>
