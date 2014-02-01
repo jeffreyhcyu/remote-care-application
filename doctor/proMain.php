e7f701fcf9954ab6e6b4942d19a2fac08ad30a22
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-02-01 15:21:12 +0000

    without id

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -44,8 +44,9 @@ while($row = mysql_fetch_array($result))
 
 $current=$_GET["w1"];
 
-$result2 = mysql_query("SELECT drug1 FROM patientDrugs WHERE id=$current");
+$result2 = mysql_query("SELECT drug1 FROM patientDrugs ");
 
+echo "<script type='text/javascript'>alert('$result2');</script>";
 
 mysql_close();
 ?>
