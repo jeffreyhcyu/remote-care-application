d788dafc02b8aacad61775852b81bfbb52204781
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-02-01 15:08:24 +0000

    move 2nd query

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -42,7 +42,7 @@ while($row = mysql_fetch_array($result))
   echo '</div>';
   }
 
-
+$result2 = mysql_query("SELECT * FROM patientDrugs ");
 
 mysql_close();
 ?>
@@ -56,7 +56,7 @@ mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
 @mysql_select_db($database);
 
 
-$result2 = mysql_query("SELECT * FROM patientDrugs ");
+
 
 
 mysql_close();
