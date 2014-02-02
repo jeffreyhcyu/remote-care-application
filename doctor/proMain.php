120548437ff3bd3f55834b3616167c8401d94d09
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-02-02 23:07:00 +0000

    change result to result 4

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -94,8 +94,8 @@ window.location.href = "proMain.php?w1=" + idNum;
 		$SQLQuery = "SELECT b.date, b.patientCurrentBPSystolic, b.patientCurrentBPDiastolic
 	    FROM patientCurrentBP AS b JOIN patientInfo AS i ON b.patientID=i.patientID WHERE i.id='$current'";
 
-		$result = mysql_query($SQLQuery);
-		$num = mysql_num_rows($result);
+		$result4 = mysql_query($SQLQuery);
+		$num = mysql_num_rows($result4);
 
 
 		//This builds an array that contains the BP values. This array is then used by the javascript to make the chart.
@@ -103,12 +103,13 @@ window.location.href = "proMain.php?w1=" + idNum;
 		$data[0] = array('day','SystolicBP','DiastolicBP');		
 		for ($i=1; $i<($num+1); $i++)
 		{
-		    $data[$i] = array(substr(mysql_result($result, $i-1, "date"), 0, 10),
-		    		(int) mysql_result($result, $i-1, "patientCurrentBPSystolic"),
-				(int) mysql_result($result, $i-1, "patientCurrentBPDiastolic") );
+		    $data[$i] = array(substr(mysql_result($result4, $i-1, "date"), 0, 10),
+		    		(int) mysql_result($result4, $i-1, "patientCurrentBPSystolic"),
+				(int) mysql_result($result4, $i-1, "patientCurrentBPDiastolic") );
 		}
 
 		echo json_encode($data);
+
 
 		mysql_close();
 
