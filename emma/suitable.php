d3c233675d18255e89927886ebf666bcab30ff3b
Emma Saragossi <emma@octa.co.uk>
2014-02-09 15:41:43 +0000

    edit

--- emma/suitable.php
+++ emma/suitable.php
@@ -1,12 +1,12 @@
 <?php
 
 $selected = $_POST['disclaimer[]'];
-if(empty($selected))
+if(!empty($selected))
 {
-header('Location: https://3yp.villocq.com/emma/newPatientDetails.php'); 
+header('Location: https://3yp.villocq.com/emma/newPatientUnsuitable.php'); 
 }
 else
 {
-header('Location: https://3yp.villocq.com/emma/newPatientUnsuitable.php'); 
+header('Location: https://3yp.villocq.com/emma/newPatientDetails.php'); 
 }
 ?>
