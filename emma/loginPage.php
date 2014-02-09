de6f17670d6762235af38f3445bbbce4f93647ed
Emma Saragossi <emma@octa.co.uk>
2014-02-09 11:30:20 +0000

    update

---
+++ emma/loginPage.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cardiac Track Pro - Log In</title>
+
+<link href="styles/style_login.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript">
+function submitform()
+{
+document.forms["jsform"].submit();
+}
+</script>
+
+<body>
+
+<div class="container">
+
+  	<header><img src="images/logo_login.png" width="960" height="340" alt=""/>
+  	</header>
+    
+    <section id="login">
+    <form id="jsform" action="login.php" method="post">
+      <label for="textfield">Username:</label>
+      <input type="text" name="username" id="username">
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password">
+      <input type="button" value="Log In" onClick="location.href='javascript: submitform()'">
+		<?php
+		session_start();
+		echo $_SESSION['loginMessage'];
+		?>
+      </form>
+    </section>
+      
+    <div class="push"></div>
+
+</div>
+
+<footer class="footer">
+	<ul>
+          <a href="siteTerms.html"><li>Site Terms</li></a>
+          <a href="aboutUs.html"><li>About Us</li></a>
+	</ul>
+</footer>    
+      
+</body>
+</html>
