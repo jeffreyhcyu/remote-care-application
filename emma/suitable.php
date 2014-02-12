056bb42ed6a8327f3b3ea68a4b81c25154f3de8b
henryfletch <henry@villocq.com>
2014-02-12 17:36:45 +0000

    testing

--- emma/suitable.php
+++ emma/suitable.php
@@ -1,12 +1,15 @@
 <?php
 
 $selected = $_POST['disclaimer[]'];
-if(!empty($selected))
-{
-header('Location: https://3yp.villocq.com/emma/newPatientUnsuitable.php'); 
-}
-else
-{
-header('Location: https://3yp.villocq.com/emma/newPatientDetails.php'); 
-}
+echo $selected;
+
+//if(!empty($selected))
+//{
+//header('Location: https://3yp.villocq.com/emma/newPatientUnsuitable.php'); 
+//}
+//else
+//{
+//header('Location: https://3yp.villocq.com/emma/newPatientDetails.php'); 
+//}
+
 ?>
