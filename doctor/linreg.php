6cef929f6ce187546ab904fc7d6d88ba8697e9d8
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-02-17 21:18:09 +0000

    test 2

--- doctor/linreg.php
+++ doctor/linreg.php
@@ -113,7 +113,7 @@
 					$daysevenquery = mysql_query("SELECT SBP FROM FraudTest WHERE id='$patient_id' AND Day=7");
 					$daysevenin = mysql_fetch_array($daysevenquery);
 
-			if(1.1*$dayone>$dayonein){
+			if(1.1*$dayone<$dayonein){
 						$patient_flag = $patient_flag+1;
 			};
 
