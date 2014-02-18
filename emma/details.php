718d2c6b537ab4cf6875c4a9a11b3565655924c9
henryfletch <henry@villocq.com>
2014-02-18 23:19:58 +0000

    changed to session vars

--- emma/details.php
+++ emma/details.php
@@ -1,62 +1,24 @@
 <?php
 
-// Configure the MySQL connection
-$server="remote.villocq.com";
-$username="3yp";
-$DBpassword="project";
-$database="tallis";
+session_start();
 
-// New MySQLi Instance
-$db = new mysqli($server,$username,$DBpassword,$database);
+//Save as temporary variables
+$_SESSION['ageGroup'] = $_POST['ageGroup'];
+$_SESSION['gender'] = $_POST['gender'];
+$_SESSION['ethnicity'] = $_POST['ethnicity'];
 
-if($_POST['formSubmit'] == "Submit")
-{
-	$errorMessage = "";
-
-		if(empty($_POST['ageGroup'])) 
-        {
-			$errorMessage .= "<li>Please select age group.</li>";
-		}
-		
-		if(empty($_POST['gender'])) 
-        {
-		$errorMessage .= "<li>Please select a sex</li>";
-		}
+echo $_SESSION['ageGroup'];
+echo '<br>';
+echo $_SESSION['gender'];
+echo '<br>';
+echo $_SESSION['ethnicity'];
 
 
-		if(empty($_POST['ethnicity'])) 
+// The user is directed to this page after completing the form. 
 
-        {
-
-		$errorMessage .= "<li>Please indicate if patient is of Black African or Black Caribbean descent.</li>";
-
-		}
+header("Location: newPatientComorbidities.html");
 
 
-$varAgeGroup = $_POST['ageGroup'];
 
-$varGender = $_POST['gender'];
-
-$varEthnicity = $_POST['ethnicity'];
-
-
-$sql = "INSERT INTO patientInfo (ageGroup, gender, ethnicity) VALUES (".
-
-							PrepSQL($varAgeGroup) . ", " .
-
-							PrepSQL($varGender) . ", " .
-
-							PrepSQL($varEthnicity) . ", " .
-
-
-			mysql_query($sql);
-
-  // The user is directed to this page after completing the form. 
-  // This code assumes that the thankyou form is in the same folder as the example form.
-			
-			header("Location: newPatientComorbidities.html");
-
-			exit();
-}
 
 ?>
