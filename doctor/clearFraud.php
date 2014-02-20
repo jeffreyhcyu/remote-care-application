474f1f55f6e22d68f240f0f14e20456983d280e5
henryfletch <henry@villocq.com>
2014-02-20 09:38:22 +0000

    removed whitespace

--- doctor/clearFraud.php
+++ doctor/clearFraud.php
@@ -1,15 +1,15 @@
-          <?php 
-              // Configure the MySQL connection
-          $username="3yp";
-          $DBpassword="project";
-          $database="tallis";
+<?php 
+// Configure the MySQL connection
+$username="3yp";
+$DBpassword="project";
+$database="tallis";
 
-          mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
-          @mysql_select_db($database);
+mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
+@mysql_select_db($database);
 
-          $current=$_GET["w1"];
+$current=$_GET["w1"];
 
-          //Perform the SQL Query
-          mysql_query("UPDATE FraudFlag SET flag=0 WHERE username = (SELECT patientID FROM patientInfo WHERE id='$current')");
+//Perform the SQL Query
+ mysql_query("UPDATE FraudFlag SET flag=0 WHERE username = (SELECT patientID FROM patientInfo WHERE id='$current')");
 
-          ?>
+ ?>
