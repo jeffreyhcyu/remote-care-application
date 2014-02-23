dc80682c044d0b3dc1bb1d80411a8bc4ef534e2f
henryfletch <henry@villocq.com>
2014-02-23 18:57:10 +0000

    fixing flag

--- doctor/proMain.php
+++ doctor/proMain.php
@@ -273,18 +273,18 @@ ID Number
         WONT WORK DUE TO TYPICAL CLIENT/SERVER SIDE DIVIDE 
         $(function clearDBfraud(){
           <?php 
-              // Configure the MySQL connection
-          $username="3yp";
-          $DBpassword="project";
-          $database="tallis";
+          // Configure the MySQL connection
+          //$username="3yp";
+          //$DBpassword="project";
+          //$database="tallis";
 
-          mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
-          @mysql_select_db($database);
+          //mysql_connect('remote.villocq.com:3306',$username,$DBpassword);
+          //@mysql_select_db($database);
 
-          $current=$_GET["w1"];
+          //$current=$_GET["w1"];
 
           //Perform the SQL Query
-          mysql_query("UPDATE FraudFlag SET flag=0 WHERE username = (SELECT patientID FROM patientInfo WHERE id='$current')");
+          //mysql_query("UPDATE FraudFlag SET flag=0 WHERE username = (SELECT patientID FROM patientInfo WHERE id='$current')");
 
           ?>
         });
