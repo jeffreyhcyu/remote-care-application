957fbdd0b8276d2638de999be9ed4c841fa81351
Emma Saragossi <emma@octa.co.uk>
2014-02-23 16:45:06 +0000

    elseif

--- emma/target.php
+++ emma/target.php
@@ -11,21 +11,21 @@ $targetSystolic = "145";
 $targetDiastolic = "85";
 }
 
-if $_SESSION['ageGroup'] == "80+" AND $_SESSION['Whitecoat'] == "No"
+elseif $_SESSION['ageGroup'] == "80+" AND $_SESSION['Whitecoat'] == "No"
 {
 $targetSystolic = "150";
 $targetDiastolic = "90";
 }
 
 
-if $_SESSION['ageGroup'] != "80+" AND $_SESSION['Whitecoat'] == "Yes"
+elseif $_SESSION['ageGroup'] != "80+" AND $_SESSION['Whitecoat'] == "Yes"
 {
 $targetSystolic = "135";
 $targetDiastolic = "85";
 }
 
 
-if $_SESSION['ageGroup'] != "80+" AND $_SESSION['Whitecoat'] == "No"
+else
 {
 $targetSystolic = "140";
 $targetDiastolic = "90";
