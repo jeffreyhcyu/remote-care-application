f98291d400b8f69541ec76620e5ec58e5431fd86
Emma Saragossi <emma@octa.co.uk>
2014-02-24 13:50:09 +0000

    revert

--- emma/comorbidity.php
+++ emma/comorbidity.php
@@ -5,13 +5,7 @@ session_start();
 if($_POST['formSubmit'] == "Submit")
 
 {
-	if($Gout == "Yes") {
-		$Gout = 'checked';
-		}
-
-	if $Gout = 'checked'{
 	$_SESSION['Gout'] = $_POST['Gout'];
-	}
 
 	$_SESSION['Heart_Failure'] = $_POST['Heart_Failure'];
 	
