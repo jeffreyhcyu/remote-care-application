1697ef9975fefe64fe51d447060ba3defb374a88
Emma Saragossi <emma@octa.co.uk>
2014-02-24 13:39:06 +0000

    testing sql

--- emma/comorbidity.php
+++ emma/comorbidity.php
@@ -3,14 +3,22 @@
 session_start();
 
 if($_POST['formSubmit'] == "Submit")
+
 {
-$_SESSION['Gout'] = $_POST['Gout'];
-
-$_SESSION['Heart_Failure'] = $_POST['Heart_Failure'];
-
-$_SESSION['Whitecoat'] = $_POST['Whitecoat'];
-
+	if($_POST['Gout'] == "Yes");
+	{
+	$_SESSION['Gout'] = $_POST['Gout'];
+	}
+	if($_POST['Heart_Failure'] == "Yes");
+	{
+	$_SESSION['Heart_Failure'] = $_POST['Heart_Failure'];
+	}
+	if($_POST['Whitecoat'] == "Yes");
+	{
+	$_SESSION['Whitecoat'] = $_POST['Whitecoat'];
+	}
 }
+
 header("Location: newPatientTargetBP.php");
 
 ?>
