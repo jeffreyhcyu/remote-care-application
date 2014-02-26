6cba5a4328eff6bb8f34817b7e88e2ce551cea9f
Emma Saragossi <emma@octa.co.uk>
2014-02-26 13:04:40 +0000

    home page

--- doctor/index.php
+++ doctor/index.php
@@ -84,9 +84,8 @@ while($row = mysql_fetch_array($result))
     
     <section>
       <h4>How to use this site:</h4>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
-    </section>
-    
+      <p>This is the <i>Cardiac Track Pro</i> portal. Here, you can monitor your patients' progress and update their treatment.</p></section>
+    <br>
     <section>
       <h4>Manage patients:</h4>
       <p>Search for an existing patient using their patient ID</p>
