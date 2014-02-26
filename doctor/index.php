661441ef630aa4865b4a4b9791a9c97fc8ce1ea4
henryfletch <henry@villocq.com>
2014-02-26 14:35:21 +0000

    bugfix

--- doctor/index.php
+++ doctor/index.php
@@ -65,6 +65,7 @@ while($row = mysql_fetch_array($result))
   //echo '<div class="Apatient" data-idNo=' . $row['id']. '>'; //inserted the data tag data-id
   //echo '<div class="Identification" data-idNo=' . $row['id']. '>';
   echo $row['patientID'] . ", id:" . $row['id'];
+  echo "<br>";
   //echo '</div>';
   //echo '</div>';
   }
