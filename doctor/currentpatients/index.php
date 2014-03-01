e8539761fb0b596ca2e1ae8348c7864605aada53
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-03-01 17:41:57 +0000

    make smaller

--- doctor/currentpatients/index.php
+++ doctor/currentpatients/index.php
@@ -52,7 +52,7 @@ header('Location: https://3yp.villocq.com/doctor');
 </div>
 <div class="about_us">
 <div class="cross">
-Close
+X
 </div>
 About Us
 <p>This is our application which tracks blood pressure over time</p>
