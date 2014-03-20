5f584c53bcd195dc7290f46e4687ce62d9185d18
mikefedosiuk <mikefedosiuk@hotmail.co.uk>
2014-03-20 16:39:29 +0000

    try a bit more

--- patient/menu.php
+++ patient/menu.php
@@ -15,9 +15,9 @@ header('Location: https://3yp.villocq.com/patient');
 
 <html>
 <head>
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=0.486; user-scalable=0;">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=0.95; user-scalable=0;">
 <title>Cardiac Track App</title>
-<link rel="stylesheet" type="text/css" href="Cardiac_Track_Style2.css">
+<link rel="stylesheet" type="text/css" href="Cardiac_Track_Style.css">
 <link rel="apple-touch-icon" href="/iphone_icon.png"/>
 </head>
 
@@ -30,8 +30,8 @@ Cardiac Track
 
 <div class="menu_option">
 <a href="update.php" style="text-decoration:none; color:white">Update
-<div class="arrow" style="position: relative; top:-30px;">
-<img src="arrow.png" width="80" height="100">
+<div class="arrow" style="position: relative; top:-15px;">
+<img src="arrow.png" >
 </a>
 </div>
 </div>
@@ -39,16 +39,16 @@ Cardiac Track
 <div class="menu_option2">
 <a href="history.php" style="text-decoration:none; color:white">
 Patient History
-<div class="arrow" style="position: relative; top:-30px;">
-<img src="arrow.png" width="80" height="100">
+<div class="arrow" style="position: relative; top:-15px;">
+<img src="arrow.png" >
 </a>
 </div>
 </div>
 
 <div class="menu_option2">
 <a href="contact.php" style="text-decoration:none; color:white; ">Doctors Contact Details 
-<div class="arrow" style="position: relative; top:-30px;">
-<img src="arrow.png" width="80" height="100">
+<div class="arrow" style="position: relative; top:-15px;">
+<img src="arrow.png" >
 </div>
 </div>
 
